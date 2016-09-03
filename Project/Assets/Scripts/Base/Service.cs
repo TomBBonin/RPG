f740aa2c9c4564260c367372ca3935d5530330e7
TomBBonin <tombbonin@gmail.com>
2016-09-03 16:05:30 -0700

    RPG Project Shell

---
+++ Project/Assets/Scripts/Base/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Service<TService> : MonoBehaviour where TService : Service<TService>
+{
+    private static TService _instance;
+
+    public static TService I { get { return _instance; } }
+
+    protected void Awake()
+    {
+        if (_instance != null)
+            throw new InvalidOperationException();
+        _instance = this as TService;
+
+        InitService();
+    }
+
+    protected abstract void InitService();
+}
---
+++ Project/Assets/Scripts/MobileApp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileApp : MonoBehaviour
+{
+    void Start()
+    {
+        DoLoaded();
+    }
+
+    private void DoLoaded() { GetComponent<Animator>().SetTrigger("DoLoaded"); }
+}
